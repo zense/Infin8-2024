72d16c7d00fa62a818a6cefdc6f0d3179842faee
Vaibhav <mittalvaibhav277@gmail.com>
2024-01-05 15:23:31 +0530

    added events

---
+++ src/content/Events.js
@@ -0,0 +1,112 @@
+const events = [
+  {
+    title: "Nritta",
+    description:
+      "Dazzling solo duo and group dance competitions bound to capture hearts and break all linguistic barriers through dance",
+    prizes: {
+      first: 3000,
+      second: 1500,
+    },
+    image: "",
+  },
+  {
+    title: "Blitz",
+    description:
+      "An unconventional debate format which does not follow the quintessential rules of the Asian and European Parliamentary system",
+    prizes: {
+      first: 3000,
+      second: 1500,
+    },
+    image: "",
+  },
+  {
+    title: "Brush Up",
+    description:
+      "Participants step into their imagination and splash their thoughts on their canvases",
+    prizes: {
+      first: 3000,
+      second: 1500,
+    },
+    image: "",
+  },
+  {
+    title: "Ramp It Up",
+    description:
+      "Designers and models line up to flaunt their beauty mesmerising the audience.",
+    prizes: {
+      first: 3000,
+      second: 1500,
+    },
+    image: "",
+  },
+  {
+    title: "League of Fanatics",
+    description:
+      "Four tracks of mind-boggling Pop  Trivia quizzes all lead to one final stage of cerebration, testing your knowledge and wit",
+    prizes: {
+      first: 3000,
+      second: 1500,
+    },
+    image: "",
+  },
+  {
+    title: "Battle of Bands",
+    description:
+      "Battalions of music enthusiasts come together to mesmerise us with their brilliant vocals and instrumental performances.",
+    prizes: {
+      first: 3000,
+      second: 1500,
+    },
+    image: "",
+  },
+  {
+    title: "Case in Point",
+    description:
+      "Explore your entrepreneurship skills by posing as the owner of a famous company and showcase what you would have done differently to change the course of the company",
+    prizes: {
+      first: 3000,
+      second: 1500,
+    },
+    image: "",
+  },
+  {
+    title: "Nukkad Naatak",
+    description:
+      "Theatrical/Drama face-off between student teams displaying their drama prowess",
+    prizes: {
+      first: 3000,
+      second: 1500,
+    },
+    image: "",
+  },
+  {
+    title: "Take the Stage",
+    description:
+      "Theatrical/Drama face-off between student teams displaying their drama prowess",
+    prizes: {
+      first: 3000,
+      second: 1500,
+    },
+    image: "",
+  },
+  {
+    title: "Sargam",
+    description:
+      "Battalions of music enthusiasts come together to mesmerise us with their brilliant vocals and instrumental performances.",
+    prizes: {
+      first: 3000,
+      second: 1500,
+    },
+    image: "",
+  },
+  {
+    title: "Codm",
+    description:
+      "Welcome to IIITBs rendition of the CODM World Championship. Battle it out in team deathmatch to claim top cash prizes. ",
+    prizes: {
+      first: 3000,
+      second: 1500,
+    },
+    image: "",
+  },
+];
