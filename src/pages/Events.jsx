7bf7952978936b025a592a93dc1ce48c38603418
Vaibhav <mittalvaibhav277@gmail.com>
2024-01-08 12:02:49 +0530

    updated heading

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -9,7 +9,7 @@ const Events = () => {
     <>
       <div className="bg-purple h-full">
         <div className="flex flex-col">
-          <h1 className="ml-2 text-4xl text-yellow title ">Events</h1>
+          <h1 className="ml-2 text-8xl text-yellow title ">Events</h1>
           <h3 className="ml-10 text-textblue register text-4xl font-normal mb-4">
             Register here
           </h3>
@@ -17,8 +17,9 @@ const Events = () => {
 
         <div className="">
           <hr style={{ border: " 1px solid black" }} />
-          <p className="my-2 games text-xl text-textdarkblue font-medium ">
-            Valorant. Stand-Up. DJ-Night. Call Of Duty. Duet Dance
+          <p className="my-2 games text-xl text-textdarkblue font-medium truncate ">
+            Valorant. Stand-Up. DJ-Night. Call Of Duty. Duet Dance Valorant.
+            Stand-Up. DJ-Night. Call Of Duty. Duet Dance Valorant.Stand-Up.
           </p>
           <hr style={{ border: " 1px solid black" }} />
         </div>
