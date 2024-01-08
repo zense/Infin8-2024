9a8159e471a0c46692556481c055693e91b4cc24
Vaibhav <mittalvaibhav277@gmail.com>
2024-01-08 15:38:36 +0530

    added responsive

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,19 +1,29 @@
-import React from "react";
-import { SocialIcon } from "react-social-icons";
 import events from "../content/Events";
 import EventCard from "../components/EventCard/EventCard";
 import "./Events.css";
 import Footer from "../components/Footer/Footer";
+import { useNavigate } from "react-router-dom";
 const Events = () => {
+  const navigate = useNavigate();
   return (
     <>
       <div className="bg-purple h-full">
-        <div className="flex flex-col">
-          <h1 className="ml-2 text-8xl text-yellow title ">Events</h1>
-          <h3 className="ml-10 text-textblue register text-4xl font-normal mb-4">
-            Register here
-          </h3>
+        {/* <div className="flex "> */}
+        <div className="flex justify-between ">
+          <div>
+            <h1 className="ml-2 text-8xl text-yellow  title ">Events</h1>
+          </div>
+          <div
+            className="bg-yellow text-red rounded-md text-center h-10 px-4 text-2xl mr-5 mt-4 cursor-pointer heading  "
+            onClick={() => navigate("/")}
+          >
+            Home
+          </div>
         </div>
+        {/* </div> */}
+        <h3 className="ml-10 text-textblue register text-4xl font-normal mb-4">
+          Register here
+        </h3>
 
         <div className="">
           <hr style={{ border: " 1px solid black" }} />
