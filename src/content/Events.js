66bc20ad7a30163dd9b583007f84e8a80133336e
Ananthakrishna-K-13 <Ananthakrishna.K@iiitb.ac.in>
2024-01-16 17:15:03 +0530

    update events

--- src/content/Events.js
+++ src/content/Events.js
@@ -422,6 +422,17 @@ const Eventss = [
       contacts: [{name:"H Sanjeev",number:"9491370927"}, {name:"Shreya Chavan",number:"9347425563"}],
     },
     {
+      title: "Pxl8",
+      description: "'Symmetry' will be the theme. Participate and capture symmetry in your shots through geometry or framing of objects, and showcase the world through an artist's eyes",
+      prizePool: 5000,
+      eventId: "26",
+      lastDate:" 11th Feb 11:59 pm",
+      rules:[
+        "Click good pics on the given theme and submit the best you feel.","Photos must be clicked using a mobile phone only by the one who is submitting.","RAW Image editing is allowed.","Basic visual editing such as cropping, lighting, shadow, hue, and saturation adjustments are allowed provided automatic filters are not used.","Either landscape or portrait mode is allowed.","The submission links will be sent to registered participants via mail.","Each participant is allowed to submit a single picture, submitting more than one picture may lead to his/her disqualification from the competition.","Results will be announced on our Instagram handle.","The decision of the organizing committee and the jury will be final.","Results will be announced on our Instagram handle https://www.instagram.com/infin8_iiitb/",
+      ],
+      contacts: [{name:"M srinivasan",number:"9353738745"}, {name:"Vaibhav Thapliyal",number:"8128818422"}],
+    },
+    {
       title: "Battle of Bands",
       description:
         "Battle of Bands is where battalions of music enthusiasts come together and strum their vocal cords against one another in a fierce battle to claim the glory of victory. Spectators will witness a plethora of notes each competing against the other to capture the hearts of dazzled listeners.",
@@ -930,13 +941,15 @@ const events = [
   }
   ,
   {
-    title: "Photo Competition",
-    description: "",
+    title: "Pxl8",
+    description: "'Symmetry' will be the theme. Participate and capture symmetry in your shots through geometry or framing of objects, and showcase the world through an artist's eyes",
     prizePool: 5000,
     eventId: "26",
     lastDate:" 11th Feb 11:59 pm",
-    spocs: ["M Srinivasan", "Vaibhav Thapliyal"],
-    contacts: ["9353738745", "8128818422"],
+    rules:[
+      "Click good pics on the given theme and submit the best you feel.","Photos must be clicked using a mobile phone only by the one who is submitting.","RAW Image editing is allowed.","Basic visual editing such as cropping, lighting, shadow, hue, and saturation adjustments are allowed provided automatic filters are not used.","Either landscape or portrait mode is allowed.","The submission links will be sent to registered participants via mail.","Each participant is allowed to submit a single picture, submitting more than one picture may lead to his/her disqualification from the competition.","Results will be announced on our Instagram handle.","The decision of the organizing committee and the jury will be final.","Results will be announced on our Instagram handle https://www.instagram.com/infin8_iiitb/","All artists will retain full ownership of their submissions but, by entering, you agree to allow us to use the entries in the promotion of the Infin8.","Any personal information collected from the participants will be used only for the purposes of administering the contest."
+    ],
+    contacts: [{name:"M Srinivasan",number:"9353738745"}, {name:"Vaibhav Thapliyal",number:"8128818422"}],
   },
 ];
 
